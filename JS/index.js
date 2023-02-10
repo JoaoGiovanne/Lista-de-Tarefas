f0bb7ee508a96a9063fa8a5fdd8c4f976aff0b2d
Giovanne Pagano <JoaoGiovanneEtec@gmail.com>
2023-02-09 22:52:06 -0300

    input date config

--- JS/index.js
+++ JS/index.js
@@ -2,19 +2,41 @@ var today = new Date();
 const current_day = today.getDate();
 const current_month = today.getMonth() + 1;
 const current_year = today.getFullYear();
-
-const term = document.getElementById('numDate');
-const date = new Date(term);
-const term_day = date.getDate();
-const term_month = date.getMonth() + 1;
-const term_year = date.getFullYear();
-
-const year = date.getFullYear();
 const button = document.getElementById('botao');
 
-// var date = today.getDate() + '-' + (today.getMonth() + 1) + '-' + today.getFullYear();
-
 button.addEventListener('click', function () {
+
+    const term = document.querySelector('#numDate').value;
+    let date = new Date(term);
+    let term_day = date.getDate() + 1;
+    let term_month = date.getMonth() + 1;
+    let term_year = date.getFullYear();
+
+
+
+    if (term_day == 31) { // mes terminado em dia 30
+        if (term_month == 4 || term_month == 6 || term_month == 9 || term_month == 11) {
+            term_day = 1;
+            term_month++;
+        }
+    }
+    if (term_day == 32) {  // mes terminado em dia 31
+        if (term_month == 1 || term_month == 3 || term_month == 5 || term_month == 7 || term_month == 8 || term_month == 10 || term_month == 12) {
+            term_day = 1;
+            term_month++;
+        }
+    }
+    // mes de fevereiro
+    if (term_month === 2) {
+        if (term_day === 29) {
+            term_day = 1;
+            term_month++;
+        }
+    }
+    if (term_month > 12) {
+        term_month = 1;
+        term_year++;
+    }
     console.log(term_day);
     console.log(term_month);
     console.log(term_year);
